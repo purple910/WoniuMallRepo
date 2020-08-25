7dafbc15ed82cffaac36e1964c16e0d66778bd6d
purple910 <2018077362@qq.com>
2020-08-25 16:26:47 +0800

    前端

---
+++ woniumall/woniumall/apps/__init__.py
@@ -0,0 +1,8 @@
+"""
+    @Time    : 2020/8/25 16:25 
+    @Author  : fate
+    @Site    : 
+    @File    : __init__.py.py
+    @Software: PyCharm
+"""
+
---
+++ woniumall/woniumall/libs/__init__.py
@@ -0,0 +1,8 @@
+"""
+    @Time    : 2020/8/25 16:26 
+    @Author  : fate
+    @Site    : 
+    @File    : __init__.py.py
+    @Software: PyCharm
+"""
+
